857dd0b54a50f22581522866e5042542e575ec68
Michael <russellyates2013@gmail.com>
2015-08-13 22:25:48 +1200

    added procfile and changed secret token in private pub yml production to match clone

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,4 +80,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  #added this code for heroku
+  Rails.application.routes.default_url_options[:host] = 'chatty73.herokuapp.com'
 end
